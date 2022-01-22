23d46b01c22200a421e9a23636bfb5960b6db17d
itzguled <itzguled55337@gmail.com>
2022-01-22 14:13:22 -0600

    timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
-var questionContainerElement = document.getElementById("question-container");
 var questionElement = document.getElementById("question");
 var questionContainerEl = document.getElementById("question-container");
 var hello = document.getElementById("intro");
+var results =document.getElementById("results")
 var answerEl = document.getElementById("answer-click");
 var startButton = document.getElementById("start-btn");
-var indexQuestion = 0
-
+var indexQuestion = 0;
+var score = 0;
+var timing = 0;
+var clear = undefined
 startButton.addEventListener("click", startQuiz);
 
 function startQuiz() {
@@ -14,96 +16,98 @@ function startQuiz() {
   intro.classList.add("hide");
   displayQuestion();
   displayQuestion();
+  timer();
+  score = 0;
 }
 
-  var questions = [
-    {
-      question: "What is the movie Heath Ledger won an Oscar for?",
-      answers: [
-        "BrokeBack Mountain",
-        "The Dark Knight",
-        "The Patriot",
-        "Candy",
-      ],
-      correctAnswer: "The Dark Knight",
-    },
-    {
-      question: "Who directed Django Unchained?",
-      answers: [
-      "Martin Scorsese", 
-      "Steve Spielberg", 
-      "George Lucas", 
-      "Quentin Tarantino"],
-      correctAnswer: "Quentin Tarantino",
-    },
-    {
-      question: "How much Oscars does Robert Deniro have?",
-      answers: [
-      "1", 
-      "2", 
-      "3", 
-      "4"],
-      correctAnswer: "4",
-    },
-    {
-      question: "Who is the Darth Vaders master?",
-      answers: [
-        "Grevious",
-        "Yoda.",
-        "Palpatine",
-        "Obi Wan",
-      ],
-      correctAnswer: "Palpatine",
-    },
-    {
-      question: "Which Star Wars Movie is the best(This is not subjective)?",
-      answers: ["Empire Strikes Back", 
-      "Revenge of th Sith", 
-      "Rise of Skywalker", 
-      "New Hope"],
-      correctAnswer: "Empire Strikes Back",
-    },
-  ];
+var questions = [
+  {
+    question: "What is the movie Heath Ledger won an Oscar for?",
+    answers: ["BrokeBack Mountain", "The Dark Knight", "The Patriot", "Candy"],
+    correctAnswer: "The Dark Knight",
+  },
+  {
+    question: "Who directed Django Unchained?",
+    answers: [
+      "Martin Scorsese",
+      "Steve Spielberg",
+      "George Lucas",
+      "Quentin Tarantino",
+    ],
+    correctAnswer: "Quentin Tarantino",
+  },
+  {
+    question: "How much Oscars does Robert Deniro have?",
+    answers: ["1", "2", "3", "4"],
+    correctAnswer: "4",
+  },
+  {
+    question: "Who is the Darth Vaders master?",
+    answers: ["General Grievous", "Yoda.", "Palpatine", "Obi Wan"],
+    correctAnswer: "Palpatine",
+  },
+  {
+    question: "Which Star Wars Movie is the best(This is not subjective)?",
+    answers: [
+      "Empire Strikes Back",
+      "Revenge of the Sith",
+      "Rise of Skywalker",
+      "New Hope",
+    ],
+    correctAnswer: "Empire Strikes Back",
+  },
+];
 
+function displayQuestion() {
+  if (indexQuestion >= questions.length) {
+    ending();
+  } else {
+    var answerClick = document.getElementById("answer-click");
+    var question = document.getElementById("question");
 
-  
+    while (answerClick.hasChildNodes()) {
+      answerClick.removeChild(answerClick.firstChild);
+    }
 
-  function displayQuestion() {
-// turn number 81 to determine turn is gonna be
- 
- var answerClick=document.getElementById("answer-click");
- var question=document.getElementById("question");
+    var i = 0;
+    while (i < 4) {
+      var listElement = document.createElement("li");
+      listElement.textContent = questions[indexQuestion].answers[i];
+      listElement.onclick = evaluateAndIncrement;
+      answerClick.appendChild(listElement);
 
- 
- 
- while (answerClick.hasChildNodes()){
-  answerClick.removeChild(answerClick.firstChild)
-}
-
-
- var i=0
- while(i < 4){
-  var listElement = document.createElement("li");
-  listElement.textContent=questions[indexQuestion].answers[i];
-  listElement.onclick=evaluateAndIncrement
-  answerClick.appendChild (listElement);
-  
-  i++
- }
- question.textContent = questions[indexQuestion].question;
- 
+      i++;
+    }
+    question.textContent = questions[indexQuestion].question;
+  }
 }
 
 function evaluateAndIncrement(event) {
-  event.preventDefault() 
-   // eventually code in here will evaluate answers as well
-   //event.target
-   indexQuestion++
-    displayQuestion();
-}
-//event.target
+  event.preventDefault();
 
-function wrong() {}
-function ending() {}
-function timer() {}
+  if (questions[indexQuestion].correctAnswer === event.target.innerHTML) {
+    score++;
+  }
+  indexQuestion++;
+  displayQuestion();
+}
+
+function ending() {
+  questionContainerEl.classList.add("hide");
+  results.classList.remove("hide");
+
+}
+function timer() {
+  let timerElement = document.querySelector("#timer > span")
+  let timerValue = timerElement.textContent
+  timing = parseInt(timerValue)
+  console.log("timer element", timerValue)
+  clear = setInterval(function() {
+  if (timing < 0){
+  clearInterval(refreshIntervalId);
+  }
+  document.querySelector("#timer > span").textContent = timing--
+
+  },1000)
+}
 function score() {}
